ca3d08617e36d2b3846f69bd6fc15d126182a24e
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-23 23:35:34 -0300

    Add Enums class for pet module

---
+++ src/main/java/com/anrry/petspot/modules/pet/Enums.java
@@ -0,0 +1,16 @@
+package com.anrry.petspot.modules.pet;
+
+public class Enums {
+  public enum Gender {
+    MALE,
+    FEMALE
+  }
+
+  public enum Species {
+    DOG,
+    CAT,
+    RABBIT,
+    BIRD,
+    OTHER
+  }
+}
