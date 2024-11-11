70097cef4ce230c522334bca1d7c41ca3f67b058
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-11-11 15:30:56 -0300

    Remove Spring Security dependency and add CORS configuration; refactor Owner class to include validation annotations and necessary fields

--- src/main/java/com/anrry/petspot/modules/owner/Owner.java
+++ src/main/java/com/anrry/petspot/modules/owner/Owner.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.anrry.petspot.modules.pet.Pet;
-import com.anrry.petspot.modules.user.User;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
@@ -14,9 +15,26 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(exclude = "pets", callSuper = true)
 @ToString(exclude = "pets", callSuper = true)
-public class Owner extends User {
+public class Owner {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  @NotBlank(message = "Name is mandatory")
+  @Column(name = "name", nullable = false, length = 100)
+  private String name;
+
+  @Email(message = "Email should be valid")
+  @NotBlank(message = "Email is mandatory")
+  @Column(name = "email", nullable = false, unique = true, length = 100)
+  private String email;
+
+  @Pattern(regexp = "^\\+?55\\s?\\(?[1-9]{2}\\)?\\s?[9]?[6-9]{1}[0-9]{3}\\-?[0-9]{4}$", message = "Phone number is invalid")
+  @NotBlank(message = "Phone number is mandatory")
+  @Column(name = "phone", nullable = false, unique = true, length = 20)
+  private String phone;
 
   @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Pet> pets = new ArrayList<>();
