64249afddb2e80fa8d7715e1ef33bf739bfb6872
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-11-27 19:57:02 -0300

    Remove credential allowance from CORS configuration for all origins

--- src/main/java/com/anrry/petspot/config/CorsConfig.java
+++ src/main/java/com/anrry/petspot/config/CorsConfig.java
@@ -16,8 +16,7 @@ public class CorsConfig {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true); // Allow credentials for specific trusted origins
+            .allowedHeaders("*");
       }
     };
   }
